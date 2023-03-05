835b8dcd9b2f092487c265a11b00107fa6abd867
cruze007 <124864748+cruze007@users.noreply.github.com>
2023-03-05 20:38:29 +0530

    cafeCLone

---
+++ script.js
@@ -0,0 +1,13 @@
+//add class navbar on navbar scroll
+
+const header = document.querySelector('.navbar');
+
+window.onscroll=function(){
+    var top=window.scrollY;
+    if(top>=100){
+        header.classList.add('navbardark');
+    }
+    else{
+        header.classList.remove('navbardark');
+    }
+}
